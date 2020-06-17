7b7e39e9fc81122f4c69715a55b639899779661d
Peter Gyory <ptgyory@gmail.com>
2020-06-17 16:47:53 -0600

    Add blur back

--- build/app.js
+++ build/app.js
@@ -1042,7 +1042,7 @@ AR.Detector = function () {
 
 AR.Detector.prototype.detect = function (image) {
   _cv__WEBPACK_IMPORTED_MODULE_0__["default"].grayscale(image, this.grey);
-  _cv__WEBPACK_IMPORTED_MODULE_0__["default"].adaptiveThreshold(this.grey, this.thres, 0, 7);
+  _cv__WEBPACK_IMPORTED_MODULE_0__["default"].adaptiveThreshold(this.grey, this.thres, 2, 7);
   this.contours = _cv__WEBPACK_IMPORTED_MODULE_0__["default"].findContours(this.thres, this.binary);
   this.candidates = this.findCandidates(this.contours, image.width * cameraParams.MIN_MARKER_PERIMETER, image.width * cameraParams.MAX_MARKER_PERIMETER, 0.05, 10);
   this.candidates = this.clockwiseCorners(this.candidates);
