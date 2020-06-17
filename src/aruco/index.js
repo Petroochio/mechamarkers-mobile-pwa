7609ba48beaf5fffa0d67bef19195a37d3a16f2d
Peter Gyory <ptgyory@gmail.com>
2020-06-17 16:59:18 -0600

    try 1

--- src/aruco/index.js
+++ src/aruco/index.js
@@ -81,7 +81,7 @@ AR.Detector = function(){
 
 AR.Detector.prototype.detect = function(image){
   CV.grayscale(image, this.grey);
-  CV.adaptiveThreshold(this.grey, this.thres, 2, 7);
+  CV.adaptiveThreshold(this.grey, this.thres, 1, 7);
   
   this.contours = CV.findContours(this.thres, this.binary);
 
